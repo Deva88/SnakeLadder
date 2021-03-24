d50e37435380949b5f063d4928659b39a6e0d74b
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 23:57:22 +0530

    Added: Checking the options for game like ladder, snake or No play

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,10 +3,33 @@ package com.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		// computation
-		int die = (int) (Math.random() * 6) + 1;
-		System.out.println("The Dice Number is " + die + ".");
+		// variables
+		final int NoPlay = 0;
+		final int Ladder = 1;
+		final int Snake = 2;
+		int PlayerPostion = 0;
 
+		// dice number generator
+		int die = (int) (Math.random() * 6) + 1;
+		System.out.println("The Dice Number is :" + die);
+
+		// option Number generator
+		int option = (int) Math.floor(Math.random() * 10) % 3;
+
+		if (option == 1) {
+			PlayerPostion = PlayerPostion + die;
+			System.out.println("Your status is Ladder");
+			System.out.println("Your Postion is :" + PlayerPostion);
+		} else if (option == 2) {
+
+			PlayerPostion = PlayerPostion - die;
+			System.out.println(" Your status is Snake");
+			System.out.println("Your Postion is :" + PlayerPostion);
+
+		} else {
+			System.out.println("Your Status is No Play :");
+			System.out.println("Your Postion is :" + PlayerPostion);
+		}
 	}
 
 }
