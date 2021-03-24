fd17ec916b94abc330ae37ed6783d4ec06e07537
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-25 00:20:44 +0530

    Added: Playing the game with Two players.

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -8,16 +8,23 @@ public class SnakeLadder {
 		final int NoPlay = 0;
 		final int Ladder = 1;
 		final int Snake = 2;
-		int PlayerPostion = 0; // initial position of player
+		int PlayerPostion1 = 0;// initial position of player
+		int PlayerPostion2 = 0;
 		int count = 0;
+		boolean Player = true;
 
 		// loop till player reach the winning spot which is 100
-		while (PlayerPostion < 100) {
+		while (PlayerPostion1 < 100 && PlayerPostion2 < 100) {
+
+			int CurrentPlayerPostion = Player ? PlayerPostion1 : PlayerPostion2;
 
 			// dice number generator
 			int die = (int) (Math.random() * 6) + 1;
-
 			count++;// counting the number of times dice was played
+
+			// turn is for player
+			System.out.println("Turn: " + (Player ? "Player 1" : "Player 2"));
+
 			System.out.println("The Dice Number is :" + die);
 
 			// option Number generator
@@ -25,31 +32,40 @@ public class SnakeLadder {
 			System.out.println("Genrat Random Postion :" + option);
 
 			if (option == 1) {
-
-				if (PlayerPostion <= 100 && (PlayerPostion + die <= 100))
-					PlayerPostion = PlayerPostion + die;
+				if (CurrentPlayerPostion <= 100 && (CurrentPlayerPostion + die <= 100))
+					CurrentPlayerPostion = CurrentPlayerPostion + die;
 				System.out.println("Your status is Ladder");
-				// printing position of player
-				System.out.println("Your Postion is :" + PlayerPostion);
+
 			}
 
 			else if (option == 2) {
-
 				// In case the player position moves below 0, then the player restarts from 0
-				if (PlayerPostion > 0 && (PlayerPostion - die) >= 0) {
-					PlayerPostion = PlayerPostion - die;
+				if (CurrentPlayerPostion > 0 && (CurrentPlayerPostion - die) >= 0) {
+					CurrentPlayerPostion = CurrentPlayerPostion - die;
 					System.out.println(" Your status is Snake");
-					System.out.println("Your Postion is :" + PlayerPostion);
+
 				}
 			}
 
 			else {
 				System.out.println("Your Status is No Play ");
-				System.out.println("Your Postion is :" + PlayerPostion);
 			}
+			// cheaking current postion player
+			System.out.println("Current Position: " + CurrentPlayerPostion);
+			if (Player)
+				PlayerPostion1 = CurrentPlayerPostion;
+			else
+				PlayerPostion2 = CurrentPlayerPostion;
+
+			if (option != Ladder)
+				Player = !Player;
+
 		}
 		// count how many times dice will Rotate starting to end
 		System.out.println("Total number of times dice is played :" + count);
+
+		// checking who has won
+		System.out.println(PlayerPostion1 >= 100 ? "Player 1 Won" : "Player 2 Won");
 	}
 
 }
