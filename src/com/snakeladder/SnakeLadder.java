78f513ad97631a4b72589e8bf97abe8036a69974
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 23:52:07 +0530

    Added: Rolled a dice

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,7 +3,9 @@ package com.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Start Position is Zero");
+		// computation
+		int die = (int) (Math.random() * 6) + 1;
+		System.out.println("The Dice Number is " + die + ".");
 
 	}
 
