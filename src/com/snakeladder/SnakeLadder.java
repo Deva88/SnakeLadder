30286ae8c13ef66cfb76eefd882f3851daee299b
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-25 00:11:58 +0530

    Added: Count the number of time dice is rolled.

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -9,12 +9,15 @@ public class SnakeLadder {
 		final int Ladder = 1;
 		final int Snake = 2;
 		int PlayerPostion = 0; // initial position of player
+		int count = 0;
 
 		// loop till player reach the winning spot which is 100
 		while (PlayerPostion < 100) {
 
 			// dice number generator
 			int die = (int) (Math.random() * 6) + 1;
+
+			count++;// counting the number of times dice was played
 			System.out.println("The Dice Number is :" + die);
 
 			// option Number generator
@@ -22,15 +25,16 @@ public class SnakeLadder {
 			System.out.println("Genrat Random Postion :" + option);
 
 			if (option == 1) {
+
 				if (PlayerPostion <= 100 && (PlayerPostion + die <= 100))
 					PlayerPostion = PlayerPostion + die;
 				System.out.println("Your status is Ladder");
-
 				// printing position of player
 				System.out.println("Your Postion is :" + PlayerPostion);
 			}
 
 			else if (option == 2) {
+
 				// In case the player position moves below 0, then the player restarts from 0
 				if (PlayerPostion > 0 && (PlayerPostion - die) >= 0) {
 					PlayerPostion = PlayerPostion - die;
@@ -44,6 +48,8 @@ public class SnakeLadder {
 				System.out.println("Your Postion is :" + PlayerPostion);
 			}
 		}
+		// count how many times dice will Rotate starting to end
+		System.out.println("Total number of times dice is played :" + count);
 	}
 
 }
