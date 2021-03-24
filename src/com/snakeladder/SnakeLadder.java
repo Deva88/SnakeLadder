7ad07e68d193096ccd4961a3a8698d785804bb5a
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 23:15:02 +0530

    Added: Single player at start position zero

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,7 +3,7 @@ package com.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Snake Ladder");
+		System.out.println("Start Position is Zero");
 
 	}
 
