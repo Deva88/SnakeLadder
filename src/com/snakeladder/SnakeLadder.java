60e292fde47e965c7639873bd5602eab01ad8eb2
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-25 00:03:44 +0530

    Added: Play till player reach the wining game

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,33 +3,42 @@ package com.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
+
 		// variables
 		final int NoPlay = 0;
 		final int Ladder = 1;
 		final int Snake = 2;
-		int PlayerPostion = 0;
+		int PlayerPostion = 0; // initial position of player
 
-		// dice number generator
-		int die = (int) (Math.random() * 6) + 1;
-		System.out.println("The Dice Number is :" + die);
+		// loop till player reach the winning spot which is 100
+		while (PlayerPostion < 100) {
 
-		// option Number generator
-		int option = (int) Math.floor(Math.random() * 10) % 3;
+			// dice number generator
+			int die = (int) (Math.random() * 6) + 1;
+			System.out.println("The Dice Number is :" + die);
 
-		if (option == 1) {
-			PlayerPostion = PlayerPostion + die;
-			System.out.println("Your status is Ladder");
-			System.out.println("Your Postion is :" + PlayerPostion);
-		} else if (option == 2) {
+			// option Number generator
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Genrat Random Postion :" + option);
 
-			PlayerPostion = PlayerPostion - die;
-			System.out.println(" Your status is Snake");
-			System.out.println("Your Postion is :" + PlayerPostion);
+			if (option == 1) {
+				PlayerPostion = PlayerPostion + die;
+				System.out.println("Your status is Ladder");
+				// printing position of player
+				System.out.println("Your Postion is :" + PlayerPostion);
 
-		} else {
-			System.out.println("Your Status is No Play :");
-			System.out.println("Your Postion is :" + PlayerPostion);
+			} else if (option == 2) {
+				// In case the player position moves below 0, then the player restarts from 0
+				if (PlayerPostion > 0 && (PlayerPostion - die) >= 0) {
+					PlayerPostion = PlayerPostion - die;
+					System.out.println(" Your status is Snake");
+					System.out.println("Your Postion is :" + PlayerPostion);
+				}
+			} else {
+				System.out.println("Your Status is No Play ");
+				System.out.println("Your Postion is :" + PlayerPostion);
+			}
 		}
-	}
 
+	}
 }
